3f081f00799066d2e15135abe2a31d4946066185
MiHawk <bbdev13@gmail.com>
2020-08-03 19:21:34 +0800

    Update config.php

--- include/config.php
+++ include/config.php
@@ -1,5 +1,3 @@
 <?php 
 if(substr($_SERVER["REQUEST_URI"], -10) == "config.php"){header("Location:./");}; 
 $data['mikhmon'] = array ('1'=>'mikhmon<|<mikhmon','mikhmon>|>aWNlbA==');
-
-$data['Daim'] = array ('1'=>'Daim!192.168.5.1','Daim@|@daim','Daim#|#amFkaGNi','Daim%CloudNet.id','Daim^cloudnet.id','Daim&Rp','Daim*10','Daim(1','Daim)','Daim=10','Daim@!@disable');
